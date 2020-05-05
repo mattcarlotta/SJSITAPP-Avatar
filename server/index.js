5c53de4760b590910230823f0668bcfcaeab8e4d
Matt <carlotta.matt@gmail.com>
2020-05-05 08:32:46 -0700

    Remove importing version

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import express from "express";
 import chalk from "chalk";
-import { version } from "../package.json";
 
 const { API, PORT } = process.env;
 
@@ -12,7 +11,7 @@ export default (app) => {
   app.listen(PORT, () => {
     console.log(
       `\n${chalk.rgb(7, 54, 66).bgRgb(38, 139, 210)(" INFO ")} ${chalk.blue(
-        `Avatar microservice is up and running at ${API} (v.${version})`
+        `Avatar microservice is up and running at ${API}`
       )}\n`
     );
   });
